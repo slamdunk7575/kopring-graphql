29d025a113e53182c6440338665705d58515a157
slamdunk7575 <slamdunk7575@gmail.com>
2024-09-03 23:35:43 +0900

    (feat) init commit

---
+++ src/main/kotlin/com/yanggang/graphql/KopringGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.yanggang.graphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringGraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringGraphqlApplication>(*args)
+}
